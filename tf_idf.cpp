8fa9f414e51afc88e43ca050f513403a806e0d90
johnnanas99 <johnnanas99@gmail.com>
2020-12-17 13:24:19 +0200

    TF_IDF base-line header/cpp files

--- tf_idf.cpp
+++ tf_idf.cpp
@@ -1,45 +1,156 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+
 #include "tf_idf.hpp"
+#include "list.hpp"
+#include "hashtable.hpp"
 
-//~~~~~~~spec~~~~~~~~
 
-Index::Index(std::string a, int dimension) {
-    id = a;
-    nt = 1;
-    dim = dimension;
-    idf = 0;
+Index::Index(int buckets){
+    hash = new hashtable<IndexObject>(buckets);
+    container = new list<IndexObject>();
+    words_counter=0;
 }
 
-Index::~Index() {}
+Index::~Index(){
+    container->emptyList(true);
+}
 
-std::string Index::getWord() { 
+void Index::insert(std::string key){
+    IndexObject *in = new IndexObject(key);
+    container->insert(in);
+    hash->insert(in);
+    words_counter++;
+}
+
+bool Index::isInside(std::string key){
+    return hash->isInside(key);
+}
+
+void Index::raiseNt(std::string key){
+    hash->getContentByKeyValue(key)->raiseNt();
+}
+
+int Index::getNt(std::string key){
+    return hash->getContentByKeyValue(key)->getNt();
+}
+
+int Index::getDim(std::string key){
+    return hash->getContentByKeyValue(key)->getDim();
+}
+
+int Index::getIdf(std::string key){
+    return hash->getContentByKeyValue(key)->getIdf();
+}
+
+//TODO ********************************************************************************************************
+void Index::fix_dim(){
+    /*
+        Set a counter to zero
+        Go to each node of all the avl_nodes
+        run set_dim(counter); on that node
+        (raise counter on for the next node)
+    */
+};
+
+//TODO ********************************************************************************************************
+void Index::fix_idf(){
+    /*
+        Go to each node of all the avl_nodes
+        run find_setIdf(words_counter); on that node
+    */
+};
+
+//~~~~~~~~~~~~~~~~~~~IndexObject~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+IndexObject::IndexObject(std::string a){
+    id = a;
+    nt = 1;
+    dim = -1;
+    idf = -0.5;
+}
+
+IndexObject::~IndexObject(){}
+std::string IndexObject::getWord(){ return id;}
+
+void IndexObject::raiseNt(){nt++;}
+int IndexObject::getNt(){return nt;}
+
+
+void IndexObject::setDim(int a){dim = a;}
+int IndexObject::getDim(){return dim;}
+
+void IndexObject::find_setIdf(int n){idf = log10(float(n)/nt);}
+float IndexObject::getIdf(){return idf;}
+
+// void IndexObject::print(){std::cout << id << std::endl;}
+
+
+//~~~~~~~~~~~~~~~~~~~json_index~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+json_index::json_index(std::string name,int buckets){
+    id = name;
+    hash = new hashtable<json_indexObject>(buckets);
+    container = new list<json_indexObject>();
+    words_counter = 0;
+}
+
+json_index::~json_index(){
+    container->emptyList(true);
+}
+
+std::string json_index::getId(){
     return id;
 }
 
-void Index::raiseNt() {
-    nt++;
+void json_index::insert(std::string key){
+    json_indexObject *in = new json_indexObject(key);
+    container->insert(in);
+    hash->insert(in);
+    words_counter++;
 }
 
-int Index::getNt() {
-    return nt;
+bool json_index::isInside(std::string key){
+    return hash->isInside(key);
 }
 
-void Index::setDim(int a) {
-    dim = a;
+void json_index::raiseCount(std::string key){
+    words_counter++;
 }
 
-int Index::getDim() {
-    return dim;
+int json_index::getCount(std::string key){
+    return hash->getContentByKeyValue(key)->getCount();
 }
 
-void Index::setIdf(float a) {
-    idf = a;
+// TODO
+void json_index::fix_Tf(){
+    /*
+        Go to each node of all the avl_nodes
+        run find_setTf(words_counter) on each node
+    */
 }
 
-float Index::getIdf() {
-    return idf;
+float json_index::getTf(std::string key){
+    return hash->getContentByKeyValue(key)->getTf();
 }
 
 
-void Index::print() {
-    std::cout << id << std::endl;
+//~~~~~~~~~~~~~~~~~~~json_indexObject~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+json_indexObject::json_indexObject(std::string a){
+    id = a;
+    count = 1;
+    tf = 0;
 }
+
+json_indexObject::~json_indexObject(){}
+std::string json_indexObject::getWord(){ return id;}
+
+void json_indexObject::raiseCount(){count++;}
+int json_indexObject::getCount(){return count;}
+
+void json_indexObject::find_setTf(int n){tf = float(count)/n;}
+float json_indexObject::getTf(){return tf;}
